42a13e73a8b2502228aa3d2cb3b31a4c7f97f4cc
liuxingyu01 <184974699@qq.com>
2022-01-21 22:29:28 +0800

    修改部分注释

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,11 +1,8 @@
-/**
-*   @description 按扭级权限校验
-*   @author Xiaohui Zeng
-*   @date 2020-05-16
-*/
 import store from '@/store'
-// 权限校验
+
+
 /**
+ * 权限校验
  * @param {Array} roleIds 权限id
  * @returns {Boolean}
  * @example v-if="checkPermission([1])"
@@ -20,7 +17,7 @@ export default function checkPermission(roleIds) {
     return flag
   } else {
     // 如果是挂在到vue全局使用  v-if="$checkPermission([1])"
-    console.error(`需要按照这个格式来设置 v-if="checkPermission([1])"`)
+    console.error(`需要按照这个格式来设置 v-if="$checkPermission([1])"`)
     return false
   }
 }
