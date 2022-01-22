97077ea641f16d6aae09499e37f5abc51f646963
liuxingyu01 <184974699@qq.com>
2022-01-22 12:07:43 +0800

    增加几个公共方法

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,59 +2,194 @@ import router from "@/router"
 
 // 判断是否为空 不为空返回数据 为空返回--
 export const isNotNull = (data, str) => {
-  if (isBlank(str)) {
-    str = "--"
-  }
-  if (data == null || data === 'null' || data === '' || data === undefined || data === 'undefined' || data === 'unknown') {
-    return str
-  } else {
-    return data
-  }
+    if (isBlank(str)) {
+        str = "--"
+    }
+    if (data == null || data === 'null' || data === '' || data === undefined || data === 'undefined' || data === 'unknown') {
+        return str
+    } else {
+        return data
+    }
 }
 
 // 判断是否为空 返回true/false
 export const isBlank = (data) => {
-  if (data == null || data === 'null' || data === '' || data === undefined || data === 'undefined' || data === 'unknown') {
-    return true
-  } else {
-    return false
-  }
+    if (data == null || data === 'null' || data === '' || data === undefined || data === 'undefined' || data === 'unknown') {
+        return true
+    } else {
+        return false
+    }
 }
 
 // 返回上一页
 export const backPage = () => {
-  router.go(-1)
+    router.go(-1)
 }
 
 
 export const localSave = (key, value) => {
-  localStorage.setItem(key, value)
+    localStorage.setItem(key, value)
 }
 
 export const localRead = (key) => {
-  return localStorage.getItem(key) || ''
+    return localStorage.getItem(key) || ''
 }
 
 export const localRemove = (key) => {
-  return localStorage.removeItem(key)
+    return localStorage.removeItem(key)
 }
 
 export const localClear = () => {
-  return localStorage.clear()
+    return localStorage.clear()
 }
 
 export const sessionSave = (key, value) => {
-  sessionStorage.setItem(key, value)
+    sessionStorage.setItem(key, value)
 }
 
 export const sessionRead = (key) => {
-  return sessionStorage.getItem(key) || ''
+    return sessionStorage.getItem(key) || ''
 }
 
 export const sessionRemove = (key) => {
-  return sessionStorage.removeItem(key)
+    return sessionStorage.removeItem(key)
 }
 
 export const sessionClear = () => {
-  return sessionStorage.clear()
+    return sessionStorage.clear()
+}
+
+
+/**
+ * 深拷贝
+ * @param obj
+ * @param cache
+ * @returns {Date|RegExp|any}
+ * @参考 https://juejin.cn/post/7031322059414175774
+ */
+export const deepClone = (obj, cache = new WeakMap()) => {
+    if (typeof obj !== 'object') return obj
+    if (obj === null) return obj
+    if (cache.get(obj)) return cache.get(obj) // 防止循环引用，程序进入死循环
+    if (obj instanceof Date) return new Date(obj)
+    if (obj instanceof RegExp) return new RegExp(obj)
+
+    // 找到所属原型上的constructor，所属原型上的constructor指向当前对象的构造函数
+    let cloneObj = new obj.constructor()
+    cache.set(obj, cloneObj) // 缓存拷贝的对象，用于处理循环引用的情况
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            cloneObj[key] = deepClone(obj[key], cache) // 递归拷贝
+        }
+    }
+    return cloneObj
+}
+
+
+/**
+ * 数组去重1
+ * @param arr
+ * @returns {any[]}
+ */
+export const dedupArr = (arr) => {
+    return [...new Set(arr)]
+}
+
+
+/**
+ * 数组去重2
+ * @param arr
+ * @returns {any[]}
+ */
+export const dedupArr2 = (arr) => {
+    return Array.from(new Set(arr))
+}
+
+
+/**
+ * 数组去重3
+ * @param arr
+ * @returns {any[]}
+ */
+export const dedupArr3 = (arr) => {
+    let res = []
+    arr.forEach(item => {
+        if (res.indexOf(item) === -1) {
+            res.push(item)
+        }
+    })
+    return res
+}
+
+
+/**
+ * 获取当前url参数
+ * @param arr
+ * @returns {any[]}
+ */
+export const getPresentUrlParams = () => {
+    // 创建一个URLSearchParams实例
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    // 把键值对列表转换为一个对象
+    return Object.fromEntries(urlSearchParams.entries());
+}
+
+
+/**
+ * 获取指定url参数
+ * @returns {any[]}
+ * @param url
+ * @测试 const user = getParams('http://www.baidu.com?user=%E9%98%BF%E9%A3%9E&age=16')
+ * @结果 { user: '阿飞', age: '16' }
+ */
+export const getUrlParams = (url) => {
+    const res = {}
+    if (url.includes('?')) {
+        const str = url.split('?')[1]
+        const arr = str.split('&')
+        arr.forEach(item => {
+            const key = item.split('=')[0]
+            const val = item.split('=')[1]
+            // 解码
+            res[key] = decodeURIComponent(val)
+        })
+    }
+    return res
+}
+
+
+/**
+ * 防抖
+ * @param fn
+ * @param delay
+ * @returns {function(...[*]=)}
+ */
+export const debounce = (fn, delay) => {
+    let timer
+    return function (...args) {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
+
+
+/**
+ * 节流
+ * @param fn
+ * @param delay
+ * @returns {function(...[*]=)}
+ */
+export const throttle = (fn, delay) => {
+    let last = 0 // 上次触发时间
+    return (...args) => {
+        const now = Date.now()
+        if (now - last > delay) {
+            last = now
+            fn.apply(this, args)
+        }
+    }
 }
