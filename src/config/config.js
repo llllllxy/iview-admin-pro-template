056ad61f6fc4d85c6b29487fa891e272aab03e0d
liuxingyu01 <184974699@qq.com>
2022-01-22 16:51:02 +0800

    增加token键

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ export const token = 'Authorization'
 
 // 主题风格
 export const themeData = {
-    themeType: 'dark', // 主题风格配置 dark(经典酷黑) 或者 light(极简雅白) 默认dark经典酷黑
+    themeType: 'dark', // 主题风格配置 dark(经典黑) 或者 light(极简白) 默认为dark经典黑
     isTabsShow: true, // 是否显示多页签 默认true
     headMaxWidth: false // 栏目头部是否通顶（最大宽度） 默认false
 }
