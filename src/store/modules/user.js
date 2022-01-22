f3ddbc615fe03243755640f7af680218c2d10e27
liuxingyu01 <184974699@qq.com>
2022-01-22 16:54:23 +0800

    token键改为从config里面获取

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,11 +1,16 @@
 import { login } from '@/api/user'
+import { resetRouter } from '@/router'
+import { token } from '@/config/config'
+
 const state = {
     // 用户信息数据
     userData: sessionStorage.getItem('userData') ? JSON.parse(sessionStorage.getItem('userData')) : {},
     // 是否登陆
     isLogin: sessionStorage.getItem('userData') ? true : false,
     // 角色权限集合
-    roles: []
+    roles: [],
+    // 用户token
+    userToken: ''
 }
 
 const mutations = {
@@ -20,6 +25,10 @@ const mutations = {
     // 修改登陆状态
     setIsLogin(state, data) {
         state.isLogin = data
+    },
+    // 修改用户token
+    setUserToken(state, data) {
+        state.userToken = data
     }
 }
 
@@ -31,10 +40,11 @@ const actions = {
             login({ userName: userName.trim(), passWord: passWord }).then(response => {
                 const { data } = response
                 sessionStorage.setItem('userData', JSON.stringify(data.userInfo))
-                sessionStorage.setItem('userToken', JSON.stringify(data.token))
+                sessionStorage.setItem(token, data.token)
                 commit('setUserData', data.userInfo)
-                // 设置登录状态为false
+                // 设置登录状态为true
                 commit('setIsLogin', true)
+                commit('setUserToken', data.token)
                 resolve()
             }).catch(error => {
                 reject(error)
@@ -62,18 +72,21 @@ const actions = {
     // 退出登录
     logout({ commit, state, dispatch }) {
         return new Promise((resolve, reject) => {
-            // commit('setToken', '')
             // 清除用户数据
             commit('setUserData', '')
             // 清楚用户权限集合
             commit('setRoles', [])
             // 设置登录状态为false
             commit('setIsLogin', false)
+            // 清除token数据
+            commit('setUserToken', '')
             // 清除缓存的用户数据
             sessionStorage.removeItem('userData')
-            sessionStorage.removeItem('userToken')
+            sessionStorage.removeItem(token)
             // 清除缓存的多页签数据
             dispatch('tagsView/setTagsView', [], { root: true })
+            // 重置路由
+            resetRouter()
             resolve()
         })
     },
