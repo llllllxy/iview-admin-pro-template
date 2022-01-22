9036a1bf8397d6ca0b45ab40482d2cd981316a00
liuxingyu01 <184974699@qq.com>
2022-01-22 16:54:02 +0800

    增加userToken

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,13 +1,14 @@
 const getters = {
-  token: state => state.user.token,
   // 是否登录
   isLogin: state => state.user.isLogin,
   // 权限id集合
   roles: state => state.user.roles,
-  // 已打开的路由数据
-  tagsViews: state => state.tagsView.tagsViews,
   // 用户信息
   userData:  state => state.user.userData,
+  // token信息
+  userToken:  state => state.user.userToken,
+  // 已打开的路由数据
+  tagsViews: state => state.tagsView.tagsViews,
   // 所有有权限的路由
   routes: state => state.permission.routes,
   // 异步路由
