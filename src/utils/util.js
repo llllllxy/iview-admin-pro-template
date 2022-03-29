3838d1a40c6e1d89356266f417de57f141a523e5
liuxingyu01 <184974699@qq.com>
2022-03-29 20:36:08 +0800

    增加获取UUID和获取UUID-V4版本公共方法

--- src/utils/util.js
+++ src/utils/util.js
@@ -397,4 +397,31 @@ export function treeToArray2 (tree, arr = []) {
 }
 
 
+/**
+ * 获取UUID
+ * @returns {string}
+ */
+export function uuid() {
+    const s = [];
+    const hexDigits = "0123456789abcdef";
+    for (let i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
 
+    return s.join("");
+}
+
+/**
+ * 获取UUID-V4版本
+ * @returns {string}
+ */
+export function uuidV4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+        const r = (Math.random() * 16) | 0; // 取整
+        const v = c === "x" ? r : (r & 0x3 | 0x8);
+        return v.toString();
+    });
+}
