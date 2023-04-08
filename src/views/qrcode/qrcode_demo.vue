57dd6208d58c35f88730eb08fb70e08af9f2f4b7
17862719591_sduRk <184974699@qq.com>
2023-04-08 09:24:24 +0800

    更新样式

--- src/views/qrcode/qrcode_demo.vue
+++ src/views/qrcode/qrcode_demo.vue
@@ -1,26 +1,28 @@
 <template>
+  <div>
     <Card>
-        <p slot="title">使用qrcodejs2生成二维码</p>
-        <div style="width: 256px;margin: 0 auto;">
-            <!--logoSrc为logo的url地址(使用require的方式)；text为需要转换为二维码的内容-->
-            <vue-qr :logoSrc="imageUrl" :text="qrcodeText" :size="256"></vue-qr>
-        </div>
+      <p slot="title">使用qrcodejs2生成二维码</p>
+      <div style="width: 256px;margin: 0 auto;">
+        <!--logoSrc为logo的url地址(使用require的方式)；text为需要转换为二维码的内容-->
+        <vue-qr :logoSrc="imageUrl" :text="qrcodeText" :size="256"></vue-qr>
+      </div>
     </Card>
+  </div>
 </template>
 <script>
-    import vueQr from 'vue-qr'
-    import logoSmall from '@/assets/main/logo-small.png'
+import vueQr from 'vue-qr'
+import logoSmall from '@/assets/main/logo-small.png'
 
-    export default {
-        name: 'qrcode_demo',
-        components: {
-            vueQr
-        },
-        data() {
-            return {
-                imageUrl: logoSmall,
-                qrcodeText: '使用qrcodejs2生成二维码'
-            }
-        }
+export default {
+  name: 'qrcode_demo',
+  components: {
+    vueQr
+  },
+  data() {
+    return {
+      imageUrl: logoSmall,
+      qrcodeText: '使用vue-qr生成二维码'
     }
+  }
+}
 </script>
